1bdef1776f6824590c1e9a24c6d5f87e94906687
ihor737 <ihor737@gmail.com>
2023-08-30 13:25:11 +0200

    one more structure changes

---
+++ HomeWork/homework_02_19.07/src/ait/HomeWork_02.java
@@ -0,0 +1,11 @@
+package ait;
+
+import java.util.Arrays;
+
+public class HomeWork_02 {
+    public static void main(String[] args) {
+        System.out.println("My name is Igor");
+        System.out.println("I am 56 years old man");
+        System.out.println("I am from Kyiv");
+    }
+}
