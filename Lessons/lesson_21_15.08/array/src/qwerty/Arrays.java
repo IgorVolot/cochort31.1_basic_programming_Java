2b0bf168b28c1cf3cb28220e12553ef10a065b7c
ihor737 <ihor737@gmail.com>
2023-08-30 11:58:40 +0200

    one more structure changes

--- Lessons/lesson_21_15.08/array/src/qwerty/Arrays.java
+++ Lessons/lesson_21_15.08/array/src/qwerty/Arrays.java
@@ -1,4 +1,4 @@
-package array;
+package qwerty;
 
 public class Arrays {
 
--- Lessons/lesson_21_15.08/array/src/qwerty/Main.java
+++ Lessons/lesson_21_15.08/array/src/qwerty/Main.java
@@ -1,4 +1,6 @@
-package array;
+package qwerty;
+
+import java.util.Arrays;
 
 public class Main {
     final static int a=10; // it's about final
@@ -6,10 +8,12 @@ public class Main {
     public static void main(String[] args) {
 //        int [] array = {4,-2,8,0,7,6,5,-5,77,4};
         int [] array = {-5,-2,0,4,4,5,6,7,8,77};
-        System.out.println(Arrays.search(array,-2)); // 1
-        System.out.println(Arrays.search(array,77));  // 8
-        System.out.println(Arrays.search(array,333));  // -1
-        System.out.println(Arrays.search(array,4));  // 0
+//        System.out.println(Arrays.search(array,-2)); // 1
+//        System.out.println(Arrays.search(array,77));  // 8
+//        System.out.println(Arrays.search(array,333));  // -1
+//        System.out.println(Arrays.search(array,4));  // 0
+
+
 
         System.out.println("============================================");
 
